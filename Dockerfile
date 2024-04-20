dc0f501cc8b1185b570a05a1d7d6fbcd12573eaa
chunhsing0921 <chunhsinig0921@gmail.com>
2024-04-20 14:58:45 +0800

    新增程式

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.12-alpine
+
+WORKDIR /app
+
+ENV secret="你的 Channel Secret"
+ENV accesstoken="你的 Access Token"
+ENV OpenAIKey="你的 OpenAI API KEY"
+ENV firebaseurl="你的 Firebase Realtime database URL"
+
+ADD . /app
+
+RUN pip install -r requirements.txt
+
+ADD firebase /usr/local/lib/python3.12/site-packages/firebase  
+
+EXPOSE 5000
+
+CMD ["python", "linebottest2.py"]
---
+++ linebottest2.py
@@ -0,0 +1,111 @@
+#from flask_ngrok import run_with_ngrok
+from flask import Flask, request
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+import os,json
+
+from openai import OpenAI
+client = OpenAI(
+    api_key = os.environ.get("OpenAIKey","None")     # 你的 OpenAI API KEY
+)
+
+from firebase import firebase
+url = os.environ.get("firebaseurl","None")    # 你的 Firebase Realtime database URL
+fdb = firebase.FirebaseApplication(url, None)
+
+secret=os.environ.get("secret","None")    # 你的 Channel Secret
+configuration = Configuration(access_token=os.environ.get("accesstoken","None"))    # 你的 Access Token
+
+Api_client=ApiClient(configuration)
+linebot_api = MessagingApi(Api_client)
+
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)
+    try:
+      
+
+        signature = request.headers['X-Line-Signature']
+
+        # get request body as text
+        body = request.get_data(as_text=True)
+        app.logger.info("Request body: " + body)
+        json_data = json.loads(body)
+        
+        tk = json_data['events'][0]['replyToken']            # 回覆的 reply token
+        timestamp = json_data['events'][0]['timestamp']      # 訊息時間戳
+        msg_type = json_data['events'][0]['message']['type'] # 訊息類型
+        # 如果是文字訊息
+        if msg_type == 'text':
+            msg = json_data['events'][0]['message']['text']  # 取出文字內容
+            print(msg)
+            chatgpt = fdb.get('/','chatgpt')                 # 讀取 Firebase 資料庫內容
+
+            if chatgpt == None:
+                messages = []       # 如果資料庫裡沒有內容，建立空串列
+            else:
+                messages = chatgpt  # 如果資料庫裡有內容，設定歷史紀錄為資料庫內容
+            print(chatgpt)
+            if msg == '!reset':
+                fdb.delete('/','chatgpt')    # 如果收到 !reset 的訊息，表示清空資料庫內容
+                #db.collection(u'/').document(u'chat').delete()
+                reply_msg = TextMessage(text='對話歷史紀錄已經清空！')
+            else:
+                messages.append({"role":"user","content":msg})  # 如果是一般文字訊息，將訊息添加到歷史紀錄裡
+                response = client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    max_tokens=128,
+                    temperature=0.5,
+                    messages=messages
+                )
+                print(response)
+                ai_msg = response.choices[0].message.content.replace('\n','')  # 移除回應裡的換行符
+                messages.append({"role":"assistant","content":ai_msg})  # 歷史紀錄裡添加回應訊息
+                fdb.put('/','chatgpt',messages)        # 使用非同步的方式紀錄訊息
+                print(ai_msg)
+              
+            linebot_api.reply_message(
+                    ReplyMessageRequest(
+                        reply_token=tk,
+                        messages=[TextMessage(text=ai_msg)]
+                    )
+                )
+        else:
+            reply_msg = TextMessage(text='你傳的不是文字訊息呦')
+            linebot_api.reply_message(
+                    ReplyMessageRequest(
+                        reply_token=tk,
+                        messages=[TextMessage(text=reply_msg)]
+                    )
+                )
+    except:
+        print('error')
+    return 'OK'
+
+if __name__ == "__main__":
+  from gevent import pywsgi
+  server = pywsgi.WSGIServer(('0.0.0.0',5000),app)
+  server.serve_forever()
+  app.run()
